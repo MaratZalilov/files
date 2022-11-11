b126a61588b6e599f3ed26ba7719ef5f9572b02a
студент2 <2@DESKTOP-QEFI0G7>
2022-11-11 11:40:58 +0300

    Добавьте файлы проекта.

---
+++ files/Program.cs
@@ -0,0 +1,51 @@
+﻿using files;
+
+WorkWithFile file = new WorkWithFile();
+void Menu()
+{
+    Console.WriteLine("Выберете пункт");
+    Console.WriteLine();
+    Console.WriteLine("1.Создать файл");
+    Console.WriteLine("2.Переместить файл");
+    Console.WriteLine("3.Создать текст");
+    Console.WriteLine("4.Удалить файл");
+
+}
+void FuncMenu()
+{
+    Menu();
+    string Enter = Console.ReadLine();
+    bool parse = int.TryParse(Enter, out int exit);
+    if (parse)
+    {
+        switch (exit)
+        {
+            case 1:
+                file.CreateFile();
+                Console.WriteLine("Вы создали файл");                
+                break;
+            case 2:
+                file.MoveFile();
+                Console.WriteLine("Вы переместили файл");
+                break;
+            case 3:               
+                file.WriteByStringInFile();
+
+                break;
+            case 4:
+                file.DeleteFile();
+                break;
+            default:
+                Console.WriteLine("Введите пункт из меню");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Для выбора пункта используйте цифры");
+    }
+}
+while (true)
+{
+    FuncMenu();
+}
---
+++ files/WorkWithFile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace files;
+
+public class WorkWithFile
+{
+    public string File1 = @"C:\Users\студент2\Desktop\одын\MyFile.txt";
+    public string File2 = @"C:\Users\студент2\Desktop\дывыа\MyFile.txt";
+    public string _enter;
+    public void CreateFile()
+    {
+        string enter = Console.ReadLine();
+        Console.WriteLine("Введите название файла");
+        string path =  $@"C:\Users\студент2\Desktop\одын\{enter}.txt";
+        _enter = enter;
+        FileInfo FileInf = new FileInfo(path);
+        if (!FileInf.Exists)
+        {
+            FileInf.Create();
+
+        }
+    }
+
+    public void MoveFile()
+    {
+        FileInfo FileInf = new FileInfo(File1);
+        if (FileInf.Exists)
+        {
+            FileInf.MoveTo(File2);
+        }
+    }
+    public void WriteByStringInFile()
+    {
+        string path =  $@"C:\Users\студент2\Desktop\одын\{_enter}.txt";
+        Console.WriteLine("Что записать?");
+        string text = Console.ReadLine();
+        FileInfo FileInfo = new FileInfo(path);
+        using(FileStream FileOut = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            byte[] buffer = Encoding.Default.GetBytes(text);
+             FileOut.Write(buffer, 0, buffer.Length);
+            Console.WriteLine("Текст записан в файл");
+        }
+    }
+    public void DeleteFile()
+    {
+        string path = $@"C:\Users\студент2\Desktop\одын\{_enter}.txt";
+        FileInfo FileInf = new FileInfo(File1);
+        if (FileInf.Exists)
+        {
+            FileInf.Delete();
+            
+        }
+    }
+}
